1df5c128f62f277dcd1ea01c5114b223a84cdacf
marco32 <marcofrosi32@gmail.com>
2017-06-23 11:40:13 +0200

    Add index and Readme

---
+++ exo.php/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Exo PHP</title>
+	  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.99.0/css/materialize.min.css">
+
+</head>
+<body class="container">
+<h3>Sommaire</h3>
+<ul>
+	<li><a href="Array/index.php">Array</a></li>
+	<li><a href="Loop/index.php">Loop</a></li>
+	<li><a href="Form/index.php">Form</a></li>
+	<li><a href="Date/index.php">Date</a></li>
+	<li><a href="Function/index.php">Function</a></li>
+	<li><a href="GlobalVariable/index.php">GlobalVariable</a></li>
+	<li><a href="ParamUrl">ParamURL</a></li>
+</ul>
+	
+</body>
+</html>
